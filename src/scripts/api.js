3ef944044e45c8d3754f8827e667081b75f00435
Leonardo Piusi <leopidev@gmail.com>
2021-07-18 04:14:56 -0300

    feat: listing products

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,13 +1,13 @@
 //This function call API and return the data
 
-export const getContent = async (page) => {
+export const getContent = async (page = 1) => {
   const url = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`;
   let content = null;
 
   await fetch(url)
   .then(response => response.json())
   .then(data => {
-    content = data.products;
+    content = data;
   })
   .catch(error => {
     console.log(error);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,27 @@
 import { getContent } from "./api.js";
 
-const listProducts = async () => {
-  const products = await getContent();
-
+const listProducts = async (page) => {
+  const content = await getContent(page);
+  const products = content.products
+  
   products.forEach(element => {
-    console.log(element)
+    const card = `
+      <div class="card" id="${element.id}">
+        <div class="image-container">
+          <img src="http://${element.image}">
+        </div>
+        <h5>${element.name}</h5>
+        <p>${element.description}</p>
+        <div class="price">
+          <span>De:</span>
+          <span>Por: R$19,99</span>
+          <span>ou ${element.installments.count} de R$9,99</span>
+        </div>
+        <button class="button">Comprar</button>
+      </div>
+    `
+    document.getElementById('products').innerHTML += card;
   });
-
 }
 
 window.onload = () => {
